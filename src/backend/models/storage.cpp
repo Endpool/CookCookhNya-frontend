2113f75bfc35ad00a95a2b428c91d57a99c2a166
Maxim Fomin <m.fomin@innopolis.university>
2025-06-19 17:57:27 +0300

    fix: api methods

--- src/backend/models/storage.cpp
+++ src/backend/models/storage.cpp
@@ -17,7 +17,7 @@ StorageSummary tag_invoke(json::value_to_tag<StorageSummary> /*tag*/, const json
 StorageDetails tag_invoke(json::value_to_tag<StorageDetails> /*tag*/, const json::value& j) {
     return {
         .name = value_to<decltype(StorageDetails::name)>(j.at("name")),
-        .ownerId = value_to<decltype(StorageDetails::ownerId)>(j.at("ownerId")),
+        .ownerId = value_to<decltype(StorageDetails::ownerId)>(j.at("id")),
     };
 }
 
@@ -27,7 +27,7 @@ void tag_invoke(boost::json::value_from_tag /*tag*/, boost::json::value& j, cons
 
 StorageCreateResponse tag_invoke(boost::json::value_to_tag<StorageCreateResponse> /*tag*/,
                                  const boost::json::value& j) {
-    return {.id = value_to<decltype(StorageSummary::id)>(j.at("id"))};
+    return {.id = value_to<decltype(StorageSummary::id)>(j.at("storageId"))};
 }
 
 } // namespace cookcookhnya::api::models::storage
