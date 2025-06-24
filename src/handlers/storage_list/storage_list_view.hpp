95ebb3191f457ba7cffce87d4d22de1a3a4b973f
Maxim Fomin <m.fomin@innopolis.university>
2025-06-24 04:03:22 +0300

    refactor: improve type_refs.hpp and handler_list.hpp (namespaces)

--- src/handlers/storage_list/storage_list_view.hpp
+++ src/handlers/storage_list/storage_list_view.hpp
@@ -6,6 +6,6 @@ namespace cookcookhnya::handlers::storageListView {
 using namespace cookcookhnya::forHandlers;
 
 void storageListButtonCallback(
-    StorageList&, CallbackQueryRef cq, const Api& bot, SMRef stateManager, StorageApiRef storageApi);
+    StorageList&, CallbackQueryRef cq, BotRef bot, SMRef stateManager, StorageApiRef storageApi);
 
 } // namespace cookcookhnya::handlers::storageListView
--- src/handlers/type_refs.hpp
+++ src/handlers/type_refs.hpp
@@ -5,30 +5,25 @@
 #include "backend/api/users.hpp"
 #include "states.hpp"
 
-#include <tg_stater/handler/event.hpp>
-#include <tg_stater/handler/handler.hpp>
 #include <tg_stater/handler/type.hpp>
-#include <tg_stater/tg_types.hpp>
 #include <tgbot/Api.h>
 #include <tgbot/types/CallbackQuery.h>
-#include <tgbot/types/InlineKeyboardButton.h>
 #include <tgbot/types/Message.h>
 
 namespace cookcookhnya::forHandlers { //*forhandlers
 
-using namespace TgBot;
-using namespace tg_stater;
 using namespace states;
-using namespace cookcookhnya::api;
 
-using ApiClientRef = const ApiClient&;
-using StorageApiRef = const StoragesApi&;
-using UserApiRef = const UsersApi&;
-using MessageRef = const Message&;
-using CallbackQueryRef = const CallbackQuery&;
-using SMRef = const StateManager&;
-using NoState = HandlerTypes::NoState;
-using AnyState = HandlerTypes::AnyState;
-using BotRef = const Api&;
+using ApiClientRef = const api::ApiClient&;
+using StorageApiRef = const api::StoragesApi&;
+using UserApiRef = const api::UsersApi&;
+
+using BotRef = const TgBot::Api&;
+using SMRef = const states::StateManager&;
+using MessageRef = const TgBot::Message&;
+using CallbackQueryRef = const TgBot::CallbackQuery&;
+
+using NoState = tg_stater::HandlerTypes::NoState;
+using AnyState = tg_stater::HandlerTypes::AnyState;
 
 } // namespace cookcookhnya::forHandlers
