d861b2c1ac1df5dfa9197697ecea72e216d31617
s3rap1s <sup.mar001@gmail.com>
2025-06-25 16:38:27 +0300

    fix: changes from improved_paging

--- src/handlers/recipes_suggestion/recipes_suggestion_list.cpp
+++ src/handlers/recipes_suggestion/recipes_suggestion_list.cpp
@@ -3,6 +3,7 @@
 #include "handlers/common.hpp"
 #include "render/recipes_suggestion/recipes_suggestion_render.hpp"
 #include "render/recipes_suggestion/select_storages_render.hpp"
+#include "render/storage_view/storage_view_render.hpp"
 #include "states.hpp"
 
 #include <sstream>
@@ -13,9 +14,11 @@ namespace cookcookhnya::handlers::recipes_suggestion {
 
 using namespace render::recipes_suggestion;
 using namespace render::select_storages;
+using namespace render::storage;
 
 void changePageAndBack(
     SuggestedRecipeList& state, CallbackQueryRef cq, BotRef bot, SMRef stateManager, ApiClientRef api) {
+    bot.answerCallbackQuery(cq.id);
 
     auto chatId = cq.message->chat->id;
     auto messageId = cq.message->messageId;
@@ -27,17 +30,18 @@ void changePageAndBack(
 
     // About pageNo - got initialised one already from state from which it goes as 1!
     if (data[0] == 'b') { // Here is quite naive implementation: if first char is b then it's "backFromSuggestedRecipes"
-        temp << data.substr(0, data.find(delimiter) + 1); // +1 is to move from space and get pure number
+        temp << data.substr(data.find(delimiter, 0) + 1,
+                            data.size()); // +1 is to move from space and get pure numberAdd commentMore actions
         int numOfStorages = 0;
         temp >> numOfStorages;
-        auto strgs = state.storageIds;
         if (numOfStorages > 1) {
             // Go to storages selection saving the storages which were chosen
-            auto message = renderStoragesSelect(strgs, userId, chatId, bot, api);
+            auto message = renderStoragesSelect(state.storageIds, userId, chatId, bot, api);
             stateManager.put(StorageSelection{.storageIds = std::move(state.storageIds), .messageId = message});
         } else {
-            stateManager.put(StorageView{strgs[0]}); // Go to the only one storage (idk what's wrong with
-                                                     // linter), index is 0 as the object is only one
+            renderStorageView(state.storageIds[0], cq.from->id, chatId, bot, api);
+            stateManager.put(StorageView{state.storageIds[0]}); // Go to the only one storage (idk what's wrong with
+                                                                // linter), index is 0 as the object is only one
         }
         return;
     }
