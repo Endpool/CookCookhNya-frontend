4f04ab721fc551c04d33c0a1519a33b2fafc8100
Maxim Fomin <62051211+Makcal@users.noreply.github.com>
2025-07-21 05:27:49 +0300

    fix: api refactor

--- src/handlers/storage/ingredients/delete.hpp
+++ src/handlers/storage/ingredients/delete.hpp
@@ -1,10 +1,11 @@
 #pragma once
 
+#include "backend/api/api.hpp>
 #include "handlers/common.hpp"
 
 namespace cookcookhnya::handlers::storage::ingredients {
 
 void handleStorageIngredientsDeletionCQ(
-    StorageIngredientsDeletion& state, CallbackQueryRef cq, BotRef& bot, SMRef stateManager, ApiClientRef api);
+    StorageIngredientsDeletion& state, CallbackQueryRef cq, BotRef& bot, SMRef stateManager, api::ApiClientRef api);
 
 } // namespace cookcookhnya::handlers::storage::ingredients
